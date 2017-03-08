77ed60d473ac347108996703e07dce69ae5d188d
hohuy <shuytlxh2@gmail.com>
2017-03-08 09:16:05 +0700

    first-init

---
+++ app/src/main/java/com/framgia/weatherforecast/ui/activities/MainActivity.java
@@ -0,0 +1,66 @@
+package com.framgia.weatherforecast.ui.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.framgia.weatherforecast.R;
+
+import static com.framgia.weatherforecast.R.id.toolbar;
+
+public class MainActivity extends AppCompatActivity
+    implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout mDrawerLayout;
+    private Toolbar mToolbar;
+    private NavigationView mNavigationView;
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setSupportActionBar(mToolbar);
+        initView();
+
+    }
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.nav_search_location:
+                //todo intent activity add location
+                break;
+            case R.id.nav_option:
+                //todo intent activity option
+                break;
+            case R.id.nav_information:
+                //todo inten activity informatiom app
+                break;
+            case R.id.nav_exit:
+                //todo inten activity informatiom app
+                break;
+            default:
+                break;
+        }
+
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    public void initView(){
+        mToolbar = (Toolbar) findViewById(toolbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+        mActionBarDrawerToggle= new ActionBarDrawerToggle(
+            this, mDrawerLayout, mToolbar, R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close);
+        mActionBarDrawerToggle.syncState();
+        mNavigationView.setNavigationItemSelectedListener(this);
+
+    }
+}
